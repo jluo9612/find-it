8986c1cbad08cefdf7d836e963594daa283546d9
Jiabei L <luojiabei@gmail.com>
2016-11-07 00:52:46 +0000

    Reorganize file structure

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var delay=10000; //1 second
 
 setTimeout(function() {
   //your code to be executed after 1 second
-  document.body.style.backgroundImage = "url('assets/scaryface.png')";
+  document.body.style.backgroundImage = "url('../assets/scaryface.png')";
 }, delay);
 
 
 function popUp () {
-  document.body.style.background = "url('assets/scaryface.png')";
+  document.body.style.background = "url('../assets/scaryface.png')";
 }
