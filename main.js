0ff953bffce3e62d555fa4af70d3a95aca62da6f
Jiabei L <luojiabei@gmail.com>
2016-11-07 00:19:31 +0000

    Design landing page and add time delay and background change

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+var delay=10000; //1 second
+
+setTimeout(function() {
+  //your code to be executed after 1 second
+  document.body.style.backgroundImage = "url('assets/scaryface.png')";
+}, delay);
+
+
 function popUp () {
-    alert("Woohoo, you found bigfoot!");
+  document.body.style.background = "url('assets/scaryface.png')";
 }
