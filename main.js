210f4867abb111089d8b5f1880b2f41c9ae19db5
Jiabei L <luojiabei@gmail.com>
2016-11-07 01:36:22 +0000

    Add sound

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ var delay=10000; //1 second
 setTimeout(function() {
   //your code to be executed after 1 second
   document.body.style.backgroundImage = "url('../assets/scaryface.png')";
+  var snd = new Audio("file.wav"); // buffers automatically when created
+  snd.play();
 }, delay);
 
 
 function popUp () {
   document.body.style.background = "url('../assets/scaryface.png')";
+  var snd = new Audio("file.wav"); // buffers automatically when created
+  snd.play();
 }
